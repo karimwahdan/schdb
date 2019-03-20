78d979eb5d84cd0ad47e4ad17ecc462bbff4ef0f
karimwahdan <46062376+karimwahdan@users.noreply.github.com>
2019-03-20 14:14:43 +0300

    Update ShiftLoader.php

--- ShiftLoader.php
+++ ShiftLoader.php
@@ -120,36 +120,12 @@ else if(empty($result)){
 
 
 $govid="";
-
-else if($_GET['querytype']=="loadhospitals")
+else if($_GET['querytype']=="loadgovid")
 	{	
-		//$shiftcode=$_POST['shiftcodepost'];
-	$sql="SELECT * FROM `hospitals` Where governomentID=$govid" ;
-	$db->sql($sql);
-$result = $db->getResult();
- $myObj= new stdClass();
-if (!empty($result)) {
- foreach($result as $row) {
-  $Out=json_encode($result);
-// echo $myJSON;
-  }
-  echo $Out;
-}
-}	
-	else if($_GET['querytype']=="loadgovs")
-	{
-		//$shiftcode=$_POST['shiftcodepost'];
-	$sql="SELECT * FROM governorates" ;
-	$db->sql($sql);
-$result = $db->getResult();
- $myObj= new stdClass();
-if (!empty($result)) {
- foreach($result as $row) {				 
- $Out=json_encode($result,JSON_UNESCAPED_UNICODE);
-// echo $myJSON;
-			  }
- echo $Out;
-}
+		$jsondata = "php://input";
+$phpjsonstring = file_get_contents( $jsondata ); // Get content of posted JSON String
+$data = json_decode( $phpjsonstring, true ); // Decoding content of posted JSON String
+$govid = $data["governomentID"];
 }
 	
 else if($_GET['querytype']=="loadcentres")
